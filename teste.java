4c0d5a5c7b180ccf61d8abfa49c01a1bd7ea6821
Luca <luca.mm01@gmail.com>
2022-06-10 19:16:30 -0300

    Aula3Edgard

---
+++ teste.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author 47127512021.1
+ *
+ */
+public class teste {
+
+	@Test
+	public void test() {
+		assertEquals(2, (1+1));
+	
+	}
+	@Test
+	public void test1() {
+		assertNotEquals(8, (3*3));
+	}
+}
+
+
